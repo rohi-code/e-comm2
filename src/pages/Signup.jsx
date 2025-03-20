a7990799c51cbc49aecbd4a43f8cd0f8a8e46949
rohi-code <rohithkonda99@gmail.com>
2025-03-20 10:53:07 +0530

    success

---
+++ src/pages/Signup.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Signup from '../components/SignUp/SignUp'
+const Signup = () => {
+  return (
+    <div>
+      <SignupPage/>
+    </div>
+  )
+}
+
+export default Signup
---
+++ src/routes/Routes.jsx
@@ -0,0 +1,12 @@
+// routes/Routes.jsx
+import LoginPage from '../pages/Login'; // Correct path to Login.jsx
+import Signup from '../pages/Signup';
+import ActivationPage from '../pages/Activation'; // Correct path to Activation.jsx
+import HomePage from '../pages/HomePage';
+import CreateProduct from '../pages/CreateProduct'
+import MyProduct from '../pages/myProduct'
+import ProductDetail from '../pages/ProductDetail';
+import Cart from '../pages/cart';
+import Profile from '../pages/profile';
+import CreateAddress from '../pages/createAddress';
+export { LoginPage,Signup,ActivationPage, HomePage,CreateProduct, MyProduct ,ProductDetail , Cart,Profile,CreateAddress }; // Named export
